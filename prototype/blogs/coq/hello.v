30e987031dd5435fc5d41a9c6ac937a536c96929
Yasuaki Kudo <yasu@tng.coop>
2023-03-12 16:54:27 +0900

    a

--- prototype/blogs/coq/hello.v
+++ prototype/blogs/coq/hello.v
@@ -1,9 +1,10 @@
-Theorem socrates_syllogism : forall {object : Type}
-  {Man Mortal : object -> Prop}
-  (Socrates : object),
-  (forall y : object, Man y -> Mortal y) ->
-   Man Socrates ->
-   Mortal Socrates.
+Theorem socrates_syllogism :
+  forall {O: Type}
+  {P Q : O -> Prop}
+  (x : O ),
+  (forall y : O, P y -> Q y) ->
+   P x->
+   Q x.
 Proof.
   intros object Man Mortal Socrates H1 H2. (* Introduce the premises and assumptions *)
   apply H1. (* Apply premise 1 (All men are mortal) to assumption 2 (Socrates is a man) to obtain Mortal Socrates *)
