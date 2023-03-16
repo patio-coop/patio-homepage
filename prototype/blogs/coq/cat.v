dcd7bf0645e0cfc77aa51bfc61c6228cfc32f096
Yasuaki Kudo <yasu@tng.coop>
2023-03-16 19:26:08 +0900

    a

--- prototype/blogs/coq/cat.v
+++ prototype/blogs/coq/cat.v
@@ -1,76 +1,42 @@
-(*
-% In the realm of Emotia, people experience two kinds of emotional connections:
-% romance and friendship. We will explore the relationships between these
-% emotional states using category theory, functors, and natural
-% transformations.
-%
-% First, let's define the inhabitants of Emotia: Alice, Bob, and Carol.
-*)
+Require Import Coq.Logic.FunctionalExtensionality.
 
-Require Import Coq.Lists.List.
-Require Import Coq.Arith.PeanoNat.
-Require Import Coq.Arith.Arith.
-
-Inductive person := Alice | Bob | Carol.
-
-(*
-% Now we will define the relationships between the inhabitants of Emotia.
-% Alice has a romantic relationship with Bob, Bob has a romantic relationship
-% with Carol, and Carol has a romantic relationship with Alice.
-*)
+Inductive person :=
+  | Alice
+  | Bob
+  | Carol.
 
 Inductive relationship : person -> person -> Type :=
-  | rel_Alice_Bob : relationship Alice Bob
-  | rel_Bob_Carol : relationship Bob Carol
-  | rel_Carol_Alice : relationship Carol Alice.
+  | rel : forall p1 p2, relationship p1 p2.
 
-(*
-% We represent the intensity of romantic feelings with love-based emotional
-% states (L) and the intensity of friendships with friendship-based emotional
-% states (F).
-*)
+Definition comp_rel {p1 p2 p3 : person} (r1 : relationship p1 p2) (r2 : relationship p2 p3) : relationship p1 p3 :=
+  rel p1 p3.
 
-Definition L (p : person) := nat.
-Definition F (p : person) := nat.
+Record Category := mkCategory {
+  Obj : Type;
+  Hom : Obj -> Obj -> Type;
+  id : forall A, Hom A A;
+  comp : forall {A B C}, Hom A B -> Hom B C -> Hom A C;
+  comp_assoc : forall A B C D (f : Hom A B) (g : Hom B C) (h : Hom C D),
+    comp (comp f g) h = comp f (comp g h);
+  id_left : forall A B (f : Hom A B), comp (id A) f = f;
+  id_right : forall A B (f : Hom A B), comp f (id B) = f
+}.
 
-(*
-% We define two functors, L_functor and F_functor, which represent how romantic
-% relationships and friendships affect emotional states.
-*)
+Definition id_rel (p : person) : relationship p p := rel p p.
 
-Definition L_functor (r : relationship Alice Bob) : L Alice -> L Bob :=
-  fun l_a => l_a.
-Definition F_functor (r : relationship Alice Bob) : F Alice -> F Bob :=
-  fun f_a => f_a.
-
-(*
-% As people in Emotia interact with each other, their emotional states change.
-% We will capture this using a natural transformation α, which represents the
-% uniform way that romantic feelings can turn into friendships.
-*)
-
-Definition alpha (p : person) (l_p : L p) : F p := l_p.
-
-(*
-% Now, we want to demonstrate that α is indeed a natural transformation. To do
-% so, we need to show that α satisfies the naturality condition.
-*)
-
-Lemma naturality_condition : forall (r : relationship Alice Bob) (l_a : L Alice),
-    F_functor r (alpha Alice l_a) = alpha Bob (L_functor r l_a).
-Proof.
-  intros r l_a.
-  unfold F_functor, L_functor, alpha.
-  destruct r.
-  reflexivity.
-Qed.
-
-(*
-% Our proof of the naturality condition shows that the transformation α from
-% romantic feelings to friendships is indeed natural. In the story of Emotia,
-% this means that the way in which romantic feelings turn into friendships is
-% consistent across all inhabitants and relationships.
-%
-% Through this exploration, we have discovered the underlying structure and
-% consistency in the realm of Emotia, where romance and friendship intertwine.
-*)
+Definition People : Category :=
+  {|
+    Obj := person;
+    Hom := relationship;
+    id := id_rel;
+    comp := @comp_rel;
+    comp_assoc := fun A B C D f g h => eq_refl;
+    id_left := fun A B (f : relationship A B) =>
+      match f with
+      | rel _ _ => eq_refl
+      end;
+    id_right := fun A B (f : relationship A B) =>
+      match f with
+      | rel _ _ => eq_refl
+      end
+  |}.
