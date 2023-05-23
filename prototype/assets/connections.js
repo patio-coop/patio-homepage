225545b10bd27e550546255972104c36c2dd7897
JairoVL <jairovillalba084@gmail.com>
2023-05-23 18:17:25 -0300

    cambios martes

---
+++ prototype/assets/connections.js
@@ -0,0 +1,17 @@
+data.connections = {
+    Argentina: [
+      "Argentina",
+      "Mexico",
+      "United States",
+      "Turkey",
+      "Greece",
+      "Croatia",
+      "Bulgaria",
+      "Japan",
+      "Brazil",
+      "Israel",
+      "New Zealand",
+    ],
+  };
+  
+  data.connections = getCountries(data.connections, data.countries);
