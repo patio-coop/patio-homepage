b0e21800265a84264e37064b1587c50c027a6490
Nicolás Dimarco <nicolasdimarco@gmail.com>
2023-06-01 15:28:25 -0300

    menu behavior fix for mobile version and style fix for desktop version

--- prototype/assets/index.js
+++ prototype/assets/index.js
@@ -135,13 +135,13 @@ document.getElementById("coops-grid-container").appendChild(countries_grid);
     $menu.classList.toggle("is-active");
   });
 
-  d.addEventListener("click", (e) => {
-    if (!e.target.matches(".menu a")) {
-      return false;
-    }
-    
-    $btnMenu.firstElementChild.classList.remove("none");
-    $btnMenu.lastElementChild.classList.add("none");
-    $menu.classList.remove("is-active");
+  let a = d.querySelectorAll(".menu a span");
+  
+  a.forEach((element,i) => {
+    a[i].addEventListener("click", (e) => { 
+        $btnMenu.firstElementChild.classList.toggle("none");
+        $btnMenu.lastElementChild.classList.toggle("none");
+        $menu.classList.toggle("is-active");
+    })
   });
 })(document);
